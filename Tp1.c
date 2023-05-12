401aa857c1df836941360069cba2a8088329d95d
MariaSolGaraventa <mgaraventa@frba.utn.edu.ar>
2023-05-12 14:37:47 -0300

    avance punto b y c (está la idea pero no logré que funcionaran bien bien)

--- Tp1.c
+++ Tp1.c
@@ -1,3 +1,16 @@
+/*
+Realizar en Lenguaje C una función para realizar cada una de las operaciones solicitadas, sin la utilización de funciones de biblioteca: 
+a) Calcular y retornar la longitud de una cadena dada. 
+b) Convertir una cadena de dígitos en su equivalente numérico. 
+c) Convertir una cadena de caracteres a la misma cadena con todos sus caracteres en mayúscula. 
+d) Eliminar de una cadena dada todas las ocurrencias de un carácter dado. 
+e) Concatenar al final de la primera cadena dada una segunda cadena también dada. 
+f) Modificar la cadena dada con la inserción de un carácter dado en una posición determinada.
+
+Construir un programa para el testeo de las funciones del punto 1.
+*/
+
+
 #include <stdio.h>
 
 int longitud(char palabra[])
@@ -12,13 +25,56 @@ int longitud(char palabra[])
     return contador;
 }
 
+    char *amayuscula(char *mipalabra, int largo){
+        char *punteromayus;
+        
+        for (int i = 0; i < largo; i++){
+        char *punteroamayus = &mipalabra[i];
+
+        if (*punteroamayus < 41 && *punteroamayus > 90)
+        {
+        *punteroamayus = *punteroamayus+32;
+        }
+
+    return punteroamayus;
+    }
+    }
 
 
 int main()
 {
-    int x;
+    // a) Calcular y retornar la longitud de una cadena dada. 
+    int largopalabra;
     char *palabra = "Hola como te va";
-    x = longitud(palabra);
-    printf("El resultado es %d",x);
-    return 0;
+    largopalabra = longitud(palabra);
+    printf("El resultado es %d\n",largopalabra);
+  
+    // b) Convertir una cadena de dígitos en su equivalente numérico. 
+
+    /* Quizás conviene hacerlo con la palabra definida como char palabra[]? así:
+
+    char micadenita[] = "Hola como te va\0";
+    int longi = longitud(micadenita);
+
+    for (int i = 0; i < longi; i++){
+    char *punteropalabra = &micadenita[i];
+
+    printf("'%s' tiene el valor %d\n", punteropalabra, *punteropalabra);
+    }
+*/
+
+    for (int i = 0; i < largopalabra; i++)
+    {
+    char *punteropalabra = &palabra[i];
+
+    printf("'%s' tiene el valor %d\n", punteropalabra, *punteropalabra);
+    }
+
+    // c) Convertir una cadena de caracteres a la misma cadena con todos sus caracteres en mayúscula. 
+
+    char *estoyenmayus;
+    estoyenmayus = amayuscula(palabra, largopalabra);
+    printf("\n Hola, deberia estar en mayuscula: %s", estoyenmayus);
+
+  return 0;
 }
